19d2020a7050ada6948bd27c792aaa8ff83719b5
YarikaAA <54471402+YarikaAA@users.noreply.github.com>
2023-07-10 12:18:43 +0300

    fix

--- backend/model/generate_recipe/infer.py
+++ backend/model/generate_recipe/infer.py
@@ -1,10 +1,10 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-tokenizer = T5Tokenizer.from_pretrained("model")
-model = T5ForConditionalGeneration.from_pretrained('model')
+tokenizer = T5Tokenizer.from_pretrained("modellll")
+model = T5ForConditionalGeneration.from_pretrained('modellll')
 
 inp = "rutte old simon genever,lemon juice,raspberries,sugar syrup,orange juice,pineapple juice,luxardo maraschino liqueur,chilled water"
-tokens = tokenizer(ingredients, return_tensors="pt")["input_ids"]
+tokens = tokenizer(inp, return_tensors="pt")["input_ids"]
 
 result = model.generate(tokens, max_new_tokens=100, num_beams=4)
 
